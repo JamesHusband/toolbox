b34d761a7fe8789362941a46807510ab653f5239
James Husband <james.husba@gmail.com>
2024-03-08 21:05:31 +0000

    convert repo to nx

---
+++ scaffolding/gpt-nx-generator/README.md
@@ -0,0 +1,11 @@
+# gpt-nx-generator
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build gpt-nx-generator` to build the library.
+
+## Running unit tests
+
+Run `nx test gpt-nx-generator` to execute the unit tests via [Jest](https://jestjs.io).
--- scaffolding/gpt-nx-generator/jest.config.ts
+++ scaffolding/gpt-nx-generator/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'nx-lib-generator',
+  displayName: 'gpt-nx-generator',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/nx-lib-generator',
+  coverageDirectory: '../../coverage/scaffolding/gpt-nx-generator',
 };
